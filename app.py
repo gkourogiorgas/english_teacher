1d8c969fae2272c792aec09f684de0e4b8527be9
Georgios Kourogiorgas <g.kourogiorgas@gmail.com>
2024-12-02 17:42:12 +0000

    fixes

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
-from streamlit_extras.bottom_container import bottom # to position the widget on the bottom 
+from streamlit_extras.bottom_container import (
+    bottom,
+)  # to position the widget on the bottom
 from streamlit_mic_recorder import mic_recorder
 from openai import OpenAI
 import io
@@ -9,25 +11,27 @@ from deepgram import (
     PrerecordedOptions,
     FileSource,
 )
+
 DEEPGRAM_MODELS = [
     "nova-2",
     "nova-2-conversationalai",
     "nova-2-phonecall",
 ]
-OPENAI_AUDIO_MODELS = [
-    "whisper-1"
-]
-OPENAI_LLMS = [
-    "gpt-4",
-    "chatgpt-4o-latest",
-    "gpt-3.5-turbo-1106"
-]
+OPENAI_AUDIO_MODELS = ["whisper-1"]
+OPENAI_LLMS = ["gpt-4", "chatgpt-4o-latest", "gpt-3.5-turbo-1106"]
 red_square = "\U0001F7E5"
 microphone = "\U0001F3A4"
 play_button = "\U000025B6"
 
+
 def init_messages():
-    st.session_state.messages = [{"role":"assistant","content": 'Hi! I am an English assistant. Talk to me and I will help you to improve!'}]
+    st.session_state.messages = [
+        {
+            "role": "assistant",
+            "content": "Hi! I am an English assistant. Talk to me and I will help you to improve!",
+        }
+    ]
+
 
 with st.sidebar:
     stt_model = st.selectbox("Select STT Model", OPENAI_AUDIO_MODELS + DEEPGRAM_MODELS)
@@ -52,21 +56,25 @@ If you believe there is a better way for the student to say what he/she is tryin
 Suggest more suitable vocabulary and phrases to the student if you think they are appropriate.
 """
 
+
 def response_generator():
     response = openai_client.chat.completions.create(
-        model = llm,
-        messages=[{"role": "system", "content": ""}]+
-            [
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ]
-            , stream=True)
+        model=llm,
+        messages=[{"role": "system", "content": ""}]
+        + [
+            {"role": m["role"], "content": m["content"]}
+            for m in st.session_state.messages
+        ],
+        stream=True,
+    )
     return response
+
 
 def text_generator(text):
     for word in text.split():
         yield word + " "
         time.sleep(0.05)
+
 
 st.title("My English Teacher")
 
@@ -82,49 +90,57 @@ if openai_api_key:
         with st.container():
             left, right = st.columns([0.9, 0.1])
             with left:
-                user_input = st.chat_input()
+                user_input_text = st.chat_input()
             with right:
-                user_input = mic_recorder(
-            key=None,
-            start_prompt=play_button + microphone,
-            stop_prompt=red_square,
-            just_once=True,
-            use_container_width=True,
-        )
-
-    if user_input:
-        if isinstance(user_input,str):
-            resp_user = user_input
-        else:
-            audio_stream = io.BytesIO(user_input['bytes'])
+                user_input_audio = mic_recorder(
+                    key=None,
+                    start_prompt=play_button + microphone,
+                    stop_prompt=red_square,
+                    just_once=True,
+                    use_container_width=True,
+                )
+    if user_input_text or user_input_audio:
+        if user_input_text:
+            resp_user = user_input_text
+        elif user_input_audio:
+            audio_stream = io.BytesIO(user_input_audio["bytes"])
             audio_stream.name = "a.mp3"
             audio_file_io = io.BufferedReader(audio_stream)
             if stt_model in OPENAI_AUDIO_MODELS:
-                transcript = client.audio.transcriptions.create(
-                model="whisper-1",
-                file=audio_file_io)
+                transcript = openai_client.audio.transcriptions.create(
+                    model="whisper-1", file=audio_file_io
+                )
                 resp_user = transcript.text
             elif stt_model in DEEPGRAM_MODELS:
                 payload: FileSource = {
-            "buffer": audio_file_io,}
+                    "buffer": audio_file_io,
+                }
 
-            #STEP 2: Configure Deepgram options for audio analysis
-            options = PrerecordedOptions(
-                model=stt_model,
-                smart_format=True,
-            )
+                # STEP 2: Configure Deepgram options for audio analysis
+                options = PrerecordedOptions(
+                    model=stt_model,
+                    smart_format=True,
+                )
 
-            # STEP 3: Call the transcribe_file method with the text payload and options
-            response = deepgram_client.listen.rest.v("1").transcribe_file(payload, options)
+                # STEP 3: Call the transcribe_file method with the text payload and options
+                response = deepgram_client.listen.rest.v("1").transcribe_file(
+                    payload, options
+                )
 
-            # STEP 4: Print the response
-            print(response.to_json(indent=4))
-            resp_user = response['results']['channels'][0]['alternatives'][0]['transcript']
+                # STEP 4: Print the response
+                print(response.to_json(indent=4))
+                resp_user = response["results"]["channels"][0]["alternatives"][0][
+                    "transcript"
+                ]
         with st.chat_message("user"):
             resp_user_stream = st.write_stream(text_generator(resp_user))
-            st.session_state.messages.append({"role":"user","content":resp_user_stream})
+            st.session_state.messages.append(
+                {"role": "user", "content": resp_user_stream}
+            )
         with st.chat_message("assistant"):
             resp_assistant = st.write_stream(response_generator())
-            st.session_state.messages.append({"role": "assistant", "content": resp_assistant})
+            st.session_state.messages.append(
+                {"role": "assistant", "content": resp_assistant}
+            )
 else:
     st.write("Please add your openai api key")
